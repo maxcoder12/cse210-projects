dc7938d88d8c25fe62a290dfc324e84e64855508
maxcoder12 <maxgblds@gmail.com>
2025-01-10 20:21:35 -0300

    Exercises 1,2,3 done

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,61 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+      Console.Write("What is your grade percentage?. In number please...");
+      int gradePercentage = int.Parse(Console.ReadLine());
+      string gradeLetter;
+      string gradeSign;
+      int gradePercentageRemainder = gradePercentage%10;
+      bool passedCourse = true;
+
+      if (gradePercentage >= 90)
+      {
+        gradeLetter = "A";
+      } 
+      else if(gradePercentage >= 80 && gradePercentage < 90)
+      {
+        gradeLetter = "B";
+      }
+      else if(gradePercentage >= 70 && gradePercentage < 80)
+      {
+        gradeLetter = "C";
+      }
+      else if (gradePercentage >= 60 && gradePercentage < 70)
+      {
+        gradeLetter = "D";
+        passedCourse = false;
+      }
+      else
+      {
+        gradeLetter = "F";
+        passedCourse = false;
+      };
+
+      if (gradePercentageRemainder >= 7)
+      {
+        gradeSign = "+";
+      }
+      else if (gradePercentageRemainder < 3)
+      {
+        gradeSign = "-";  
+      }
+      else
+      {
+        gradeSign = "";
+      };
+      
+      if (passedCourse && (gradePercentage >= 70 && gradePercentage <= 93))
+      {
+        Console.Write($"Congratulation, you passed the course with a '{gradeLetter + gradeSign}' grade!!!");
+      }
+      else if (passedCourse == false && (gradePercentage < 70 && gradePercentage >= 60))
+      {
+        Console.Write($"Unfortunately, you failed the course with a '{gradeLetter + gradeSign}' grade.");
+      }
+      else
+      {
+        Console.Write($"Unfortunately, you failed the course with a '{gradeLetter}' grade.");
+      };
+      
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+      Random randomGenerator = new Random();
+      int userGuess;
+      Console.WriteLine("Do you want to play the Magic Number game?");
+      string userReponse = Console.ReadLine();
+      while (userReponse == "yes")
+      {
+        int attemps = 0;
+        int magicNumber = randomGenerator.Next(1, 10);
+        Console.WriteLine("What is the magic number?");
+        do 
+        {
+        Console.Write("What is your guess? ");
+        userGuess = int.Parse(Console.ReadLine());
+
+        if (userGuess < magicNumber)
+        {
+          Console.WriteLine("Higher");
+          attemps++;
+          Console.WriteLine($"Attemps: {attemps}");
+        }
+        else if (userGuess > magicNumber)
+        {
+          Console.WriteLine("Lower");
+          attemps++;
+          Console.WriteLine($"Attemps: {attemps}");
+        }
+        else
+        {
+          Console.WriteLine($"You guessed it! The magic number was {userGuess}");
+          attemps++;
+          Console.WriteLine($"Attemps: {attemps}");
+          Console.WriteLine("Do you want to continue playing the Magic Number game?");
+          userReponse = Console.ReadLine();
+        };
+        } while (userGuess != magicNumber);
+      };
+
+      Console.WriteLine("Good game! Bye!");
     }
 }
